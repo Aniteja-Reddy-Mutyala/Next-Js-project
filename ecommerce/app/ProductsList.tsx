73553cb323c30c816cda85d7526c2a98916cdb1d
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-12-11 15:17:44 -0800

    Used Link component of nextjs

--- ecommerce/app/ProductsList.tsx
+++ ecommerce/app/ProductsList.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Product } from "./product-data";
 export default function ProductsList({products}:{products:Product[]}){
   return(
     <div>
         {products.map((product)=>(
             <>
+           <Link href="/product-detail">
             <div key={product.id}>{product.name}</div>
             <Image src={'/'+product.imageUrl} alt={product.name} width={150} height={150}/>
             <p> {product.description}</p>
             <span>${product.price}</span>
-            
+            </Link>
             </>
         ))}
     </div>
