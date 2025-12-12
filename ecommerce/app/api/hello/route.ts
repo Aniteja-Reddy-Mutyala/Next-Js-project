d0f6eca2a6f732ed1d739dea1afa945fd340175f
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-12-11 22:52:21 -0800

    Created a list endpoint for products

--- ecommerce/app/api/hello/route.ts
+++ ecommerce/app/api/hello/route.ts
@@ -1,6 +1,12 @@
-export async function GET(){
-    return new Response('Hello from a next js route handler',{
-        status:200,
-        statusText:"Done!!!"
-    })
-} 
+export async function GET() {
+  return new Response("Hello from a next js route handler", {
+    status: 200,
+    statusText: "Done!!!",
+  });
+}
+
+export async function POST() {
+  return new Response("This is a post request", {
+    status: 200,
+  });
+}
