aba1bf8aa9313d41bfa2b5b33e0985c861a90df2
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-12-11 16:24:51 -0800

    Created a sj]hopping cart list

--- ecommerce/app/cart/page.tsx
+++ ecommerce/app/cart/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+import { useState } from "react"
+import { products } from "../product-data"
+import Link from "next/link"
 export default function CartPage(){
+    const[ cartIds,setCartIds]=useState(['123','345'])
+    const cartProducts=cartIds.map(id=>products.find(p=>p.id===id))
     return(
         <>
-        <h1>Cart</h1>
+        <h1>Shopping Cart</h1>
+        {cartProducts.map(product=>(
+            <Link key={product?.id} href={'products/'+product?.id}>
+                <h3>{product?.name?product.name:"Name doesn't exist"}</h3>
+                <p>${product?.price}</p>
+
+            </Link>
+        ))}
         </>
     )
 }
