7b62b2be46d61ff466cdbe0ae0b65b3beab681b1
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-12-11 21:43:19 -0800

    Styling

--- ecommerce/app/products/[id]/page.tsx
+++ ecommerce/app/products/[id]/page.tsx
@@ -6,10 +6,15 @@ export default function ProductDetailPage({params}:{params:{id:string}}){
         return <NotFound/>
     }
     return(
-        <>
-        <h1>{product?.name}</h1>
-        <p>{product?.description}</p>
-        
-        </>
+        <div className="container mx-auto p-8 flex flex-col md:flex-row">
+            <div className="md:w-1/2 mb-4 md:mb-0 md:mr-8">
+        <img src={'/'+ product.imageUrl} alt={product.name} className="w-full h-auto rounded-lg shadow-md"/>
+            </div>
+         <div className="md:w-1/2">
+        <h1 className="text-4xl font-bold mb-4 ">{product?.name}</h1>
+        <p className="">{product?.description}</p>
+        <p className="text-2sm text-yellow-600 mb-4">${product?.price}</p>
+        </div>
+        </div>
     )
 }
