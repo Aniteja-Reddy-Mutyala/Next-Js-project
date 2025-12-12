5bdc8527a955978f81263f716776d35d796b0812
Aniteja Reddy Mutyala <aniteja.reddy@gmail.com>
2025-12-11 20:00:16 -0800

    Created a 404 not found page

--- ecommerce/app/products/[id]/page.tsx
+++ ecommerce/app/products/[id]/page.tsx
@@ -1,6 +1,10 @@
+import NotFound from "@/app/not-found"
 import { products } from "@/app/product-data"
 export default function ProductDetailPage({params}:{params:{id:string}}){
     const product=products.find(p=>p.id===params.id)
+    if(!product){
+        return <NotFound/>
+    }
     return(
         <>
         <h1>{product?.name}</h1>
